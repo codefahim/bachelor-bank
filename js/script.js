ad8570d54631bf3227bc4401e70a8e15dc8b6564
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-01-26 20:04:05 +0600

    login update

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ const loginBtn = document.getElementById("login");
 loginBtn.addEventListener('click', function() {
     const sectionhide = document.getElementById('loginSection');
     sectionhide.style.display = 'none';
+    const transectionArea = document.getElementById('transaction');
+    transectionArea.style.display = 'block';
 });
