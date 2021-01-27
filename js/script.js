62804ce6c3e3f1195e57d0ff6290cd5cd7b44a3d
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-01-27 10:40:04 +0600

    Project Completed

--- js/script.js
+++ js/script.js
@@ -9,45 +9,41 @@ loginBtn.addEventListener('click', function() {
     transectionArea.style.display = 'block';
 });
 
-
-// for collect data from deposit btn and more;
 const deposit = document.getElementById('deposit');
 deposit.addEventListener('click', function() {
-    // deposite value add and empty input 
+
     const depositNumber = getInputNumber('depositAmount');
 
+    showingAmount('currentBalance', depositNumber);
+    showingAmount('currentDeposit', depositNumber);
 
-    // balance check and add with deposit 
-    function totalAm(id, depositNumber) {
-        if (depositNumber >= 0) {
-            const current = document.getElementById(id).innerText;
-            const currentNumber = parseFloat(current);
-            const BalanceNow = currentNumber + depositNumber;
-            document.getElementById(id).innerText = BalanceNow;
-        }
-
-    }
-
-    totalAm('currentBalance', depositNumber);
-    totalAm('currentDeposit', depositNumber);
     document.getElementById('depositAmount').value = " ";
 });
 
-// /
-// /
-// /
-// /
-// /
+
+
 // withdraw part start from here
 const addWithdraw = document.getElementById('withdraw');
 addWithdraw.addEventListener('click', function() {
     const withdrawAmount = getInputNumber('inputWithdraw');
-    const currentWithdraw = document.getElementById('currentWithdraw').innerText;
-    const presentWithdraw = parseFloat(currentWithdraw);
+    showingAmount('currentWithdraw', withdrawAmount);
+    showingAmount('currentBalance', -1 * withdrawAmount);
+    document.getElementById('inputWithdraw').value = " ";
 
 
 
 })
+
+
+function showingAmount(id, depositNumber) {
+    if (depositNumber >= -9999999999999999999) {
+        const current = document.getElementById(id).innerText;
+        const currentNumber = parseFloat(current);
+        const BalanceNow = currentNumber + depositNumber;
+        document.getElementById(id).innerText = BalanceNow;
+    }
+
+}
 
 function getInputNumber(id) {
     const inputNumber = document.getElementById(id).value;
