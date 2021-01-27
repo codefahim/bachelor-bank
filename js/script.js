010a7827ce03aef94ac7141c2b6e3262780edb4e
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-01-27 09:46:24 +0600

    Deposit System update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// for collect data from login btn and display none;
+
+
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener('click', function() {
     const sectionhide = document.getElementById('loginSection');
@@ -5,3 +8,49 @@ loginBtn.addEventListener('click', function() {
     const transectionArea = document.getElementById('transaction');
     transectionArea.style.display = 'block';
 });
+
+
+// for collect data from deposit btn and more;
+const deposit = document.getElementById('deposit');
+deposit.addEventListener('click', function() {
+    // deposite value add and empty input 
+    const depositNumber = getInputNumber('depositAmount');
+
+
+    // balance check and add with deposit 
+    function totalAm(id, depositNumber) {
+        if (depositNumber >= 0) {
+            const current = document.getElementById(id).innerText;
+            const currentNumber = parseFloat(current);
+            const BalanceNow = currentNumber + depositNumber;
+            document.getElementById(id).innerText = BalanceNow;
+        }
+
+    }
+
+    totalAm('currentBalance', depositNumber);
+    totalAm('currentDeposit', depositNumber);
+    document.getElementById('depositAmount').value = " ";
+});
+
+// /
+// /
+// /
+// /
+// /
+// withdraw part start from here
+const addWithdraw = document.getElementById('withdraw');
+addWithdraw.addEventListener('click', function() {
+    const withdrawAmount = getInputNumber('inputWithdraw');
+    const currentWithdraw = document.getElementById('currentWithdraw').innerText;
+    const presentWithdraw = parseFloat(currentWithdraw);
+
+
+
+})
+
+function getInputNumber(id) {
+    const inputNumber = document.getElementById(id).value;
+    const inputAmount = parseFloat(inputNumber);
+    return inputAmount;
+}
